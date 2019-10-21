f1775c16dfc4b6a83d38e271d092ec427aed2dbe
user_name <email_id>
2019-10-20 20:28:32 -0700

    libstatic.a

---
+++ week6/code/staticlib/base.c
@@ -0,0 +1,14 @@
+#include "liba.h"
+#include <stdio.h>
+void show(int *a,int n)
+{
+	int i;
+	
+	for(i = 0;i<n;i++)
+	{
+		printf("%4d   :   %4d\n",i,a[i]);
+	}
+}
+void init(int *a,int n)
+{
+}
---
+++ week6/code/staticlib/localtest.c
@@ -0,0 +1,9 @@
+#include "liba.h"
+#include <stdio.h>
+int main()
+{
+	int a[5] = {12,4,5,6,99};
+	show(a,5);
+	printf("最大值为：%d\n",max(a,5));
+	printf("和为：%d\n",sum(a,5));
+}
